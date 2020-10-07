912e778f2f4400dec0e3393da705dc75fedff1fb
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-07 21:04:46 +0530

    UC 2 Code added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -21,3 +21,19 @@ attendance
 }
 
 main
+
+
+isFullTime=1;
+empRatePerHr=20;
+randomCheck=$((RANDOM%2))
+
+
+if [ $isFullTime -eq $randomCheck ];
+ then
+       empHrs=8;
+
+ else
+       empHrs=0;
+fi
+
+salary=$(($empHrs * $empRatePerHr));
