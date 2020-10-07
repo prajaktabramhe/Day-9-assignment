167477e5dac59519c1fd241bc45e48879e07b9a6
prajaktabramhe <prajakta14bramhe@gmail.com>
2020-10-07 20:37:23 +0530

    UC 1 code added

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,3 +1,23 @@
 #!/bin/bash -x
 
 echo "Welcome to Employee Wage Computation Program";
+
+function attendance()
+{
+	Random=$((RANDOM%2))
+	if [ $Random -eq 1 ]
+	 then
+		echo "Employee is present";
+	 else
+		echo "Employee is absent";
+	fi
+}
+
+function main()
+{
+
+attendance
+
+}
+
+main
